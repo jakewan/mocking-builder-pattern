4fbc374fc4e0a15e89a18157787eb3e615c7f466
Jacob Wan <jacobwan840@gmail.com>
2022-06-03 12:21:38 -0700

    Add unit test; improve testability

--- src/bin/use-builder.rs
+++ src/bin/use-builder.rs
@@ -1,9 +1,36 @@
-use mocking_builder_pattern::{process, ProductionBuilder};
+use mocking_builder_pattern::{process, Builder, BuilderWrapper, ProductionBuilder};
+
+struct ProductionBuilderWrapper<'a> {
+    builder: ProductionBuilder<'a>,
+}
+
+impl<'a> BuilderWrapper for ProductionBuilderWrapper<'a> {
+    fn set_foo(&mut self, value: &str) {
+        self.builder.with_foo(value);
+    }
+
+    fn set_bar(&mut self, value: &str) {
+        self.builder.with_bar(value);
+    }
+
+    fn set_baz(&mut self, value: &str) {
+        self.builder.with_baz(value);
+    }
+
+    fn set_quux(&mut self, value: &str) {
+        self.builder.with_quux(value);
+    }
+
+    fn to_string(self) -> String {
+        self.builder.to_string()
+    }
+}
 
 fn main() {
     // Provide a builder instance to `process` as a dependency
     let builder = ProductionBuilder::new();
-    let result = process(builder);
+    let builder_wrapper = ProductionBuilderWrapper { builder };
+    let result = process(builder_wrapper);
     if result.len() > 0 {
         println!("{}", result);
     } else {
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 use std::collections::HashMap;
 
+#[cfg(test)]
+use mockall::{automock, predicate::*};
+
 pub trait Builder {
-    fn with_foo(self, value: &str) -> Self;
-    fn with_bar(self, value: &str) -> Self;
-    fn with_baz(self, value: &str) -> Self;
-    fn with_quux(self, value: &str) -> Self;
+    fn with_foo(&mut self, value: &str) -> &Self;
+    fn with_bar(&mut self, value: &str) -> &Self;
+    fn with_baz(&mut self, value: &str) -> &Self;
+    fn with_quux(&mut self, value: &str) -> &Self;
     /// To be called at the end of a chained sequence of method calls.
     ///
     /// This consumes self in the process, so the builder may not be used
@@ -25,26 +28,26 @@ impl<'a> ProductionBuilder<'a> {
 }
 
 impl<'a> ProductionBuilder<'a> {
-    fn with_key(mut self, key: &'a str, value: &str) -> Self {
+    fn with_key(&mut self, key: &'a str, value: &str) -> &Self {
         self.items.insert(key, String::from(value));
         self
     }
 }
 
 impl<'a> Builder for ProductionBuilder<'a> {
-    fn with_foo(self, value: &str) -> Self {
+    fn with_foo(&mut self, value: &str) -> &Self {
         self.with_key("foo", value)
     }
 
-    fn with_bar(self, value: &str) -> Self {
+    fn with_bar(&mut self, value: &str) -> &Self {
         self.with_key("bar", value)
     }
 
-    fn with_baz(self, value: &str) -> Self {
+    fn with_baz(&mut self, value: &str) -> &Self {
         self.with_key("baz", value)
     }
 
-    fn with_quux(self, value: &str) -> Self {
+    fn with_quux(&mut self, value: &str) -> &Self {
         self.with_key("quux", value)
     }
 
@@ -75,13 +78,47 @@ impl<'a> Builder for ProductionBuilder<'a> {
     }
 }
 
-/// Takes a Builder implementation and returns the result of chained sequence
+#[cfg_attr(test, automock)]
+pub trait BuilderWrapper {
+    fn set_foo(&mut self, value: &str);
+    fn set_bar(&mut self, value: &str);
+    fn set_baz(&mut self, value: &str);
+    fn set_quux(&mut self, value: &str);
+    fn to_string(self) -> String;
+}
+
+/// Takes a BuilderWrapper and returns the result of a sequence
 /// of method calls.
-pub fn process<A: Builder>(builder: A) -> String {
-    builder
-        .with_foo("some foo")
-        .with_bar("some bar")
-        .with_baz("some baz")
-        .with_quux("some quux")
-        .to_string()
+pub fn process<A: BuilderWrapper>(mut builder_wrapper: A) -> String {
+    builder_wrapper.set_foo("some foo");
+    builder_wrapper.set_bar("some bar");
+    builder_wrapper.set_baz("some baz");
+    builder_wrapper.set_quux("some quux");
+    builder_wrapper.to_string()
+}
+
+#[test]
+fn test_process() {
+    let mut mock_wrapper = MockBuilderWrapper::new();
+    mock_wrapper
+        .expect_set_foo()
+        .with(eq("some foo"))
+        .return_once(|_| ());
+    mock_wrapper
+        .expect_set_bar()
+        .with(eq("some bar"))
+        .return_once(|_| ());
+    mock_wrapper
+        .expect_set_baz()
+        .with(eq("some baz"))
+        .return_once(|_| ());
+    mock_wrapper
+        .expect_set_quux()
+        .with(eq("some quux"))
+        .return_once(|_| ());
+    mock_wrapper
+        .expect_to_string()
+        .return_once(|| String::from("awesome!"));
+    let result = process(mock_wrapper);
+    assert_eq!(result, String::from("awesome!"));
 }
